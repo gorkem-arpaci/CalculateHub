e520bff77f6cef6e03c21c37a9dfcfd39d6af44f
gorkem-arpaci <gorkem.arpaci@hotmail.com>
2025-08-02 01:37:29 +0300

    Initial Commit

---
+++ CalculateHub/CalculateHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateHubApp.swift
+//  CalculateHub
+//
+//  Created by Görkem Arpacı on 2.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculateHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
